c492d1cf518c01d1f8afb6bf55840c1513c64e48
Mark Anthony Hailwood <54316459+MarkAHailwood@users.noreply.github.com>
2020-11-08 13:55:14 +0000

    route to /*

--- src/router.js
+++ src/router.js
@@ -15,6 +15,7 @@ const router = new VueRouter({
   linkActiveClass: 'is-active',
   scrollBehavior: (to, from, savedPosition) => ({ y: 0 }),
   routes: [
+    {path: '', name: 'indexPage', component: Category},
     {path: '/readme', name: 'readme', component: Readme},
     {path: '/search', name: 'category', component: Category},
     {path: '*', component: NotFound}
